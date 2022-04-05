f5784afe8fa016807188590cb4d840cf4d1de748
lau truong <lau.td@amanotes.com>
2022-04-05 15:41:45 +0700

    First Commit

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,84 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
+
+@Injectable()
+export class BookmarkService {
+  constructor(private prisma: PrismaService) {}
+  getBookmarks(userId: number) {
+    return this.prisma.bookmark.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+  }
+
+  getBookmarkById(userId: number, bookmarkId: number) {
+    return this.prisma.bookmark.findFirst({
+      where: {
+        id: bookmarkId,
+        userId: userId,
+      },
+    });
+  }
+
+  async createBookmark(userId: number, dto: CreateBookmarkDto) {
+    const bookmark = await this.prisma.bookmark.create({
+      data: {
+        userId,
+        ...dto,
+      },
+    });
+
+    return bookmark;
+  }
+
+  async editBookmarkById(
+    userId: number,
+    bookmarkId: number,
+    dto: EditBookmarkDto,
+  ) {
+    // get the bookmark by id
+    const bookmark = await this.prisma.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    });
+
+    // check if the user owns the bookmark
+    if (!bookmark || bookmark.userId != userId) {
+      throw new ForbiddenException('Access to resources denied');
+    }
+
+    // modify the bookmark
+
+    return this.prisma.bookmark.update({
+      where: {
+        id: bookmarkId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async deleteBookmarkById(userId: number, bookmarkId: number) {
+    // get the bookmark by id
+    const bookmark = await this.prisma.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    });
+
+    // check if the user owns the bookmark
+    if (!bookmark || bookmark.userId != userId) {
+      throw new ForbiddenException('Access to resources denied');
+    }
+
+    await this.prisma.bookmark.delete({
+      where: {
+        id: bookmarkId,
+      },
+    });
+  }
+}
